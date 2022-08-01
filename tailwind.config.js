8540831840fa50781ecec3efc75c696dddc56593
Aaron Hope <aghopecode@gmail.com>
2022-08-01 00:25:30 -0700

    lesson-5 (Tailwind config and theme)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/index.html'
-  ],
+  content: ['./public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883'
+        }
+      }
+    },
   },
   plugins: [],
 }
